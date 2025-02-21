788304f78a158c3fe28d43b72d9293c72f163739
Ibrahim5002 <95931557+Ibraheem5002@users.noreply.github.com>
2025-02-21 13:12:53 +0500

    Add files via upload

---
+++ 5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+using namespace std;
+
+class ValidateString
+{
+private:
+    static char Character[52];
+    char Word[100];
+public:
+};
+char ValidateString :: Character[52] = {'a', 'A', 'b', 'B', 'c', 'C', 'd', 'D', 'e', 'E', 'f', 'F', 'g', 'G', 'h', 'H', 'i', 'I', 'j', 'J', 'k', 'K', 'l', 'L', 'm', 'M', 'n', 'N', 'o', 'O', 'p', 'P', 'q', 'Q', 'r', 'R', 's', 'S', 't', 'T', 'u', 'U', 'v', 'V', 'w', 'W', 'x', 'X', 'y', 'Y', 'z', 'Z'};
